e19772ef4abac95dc471761db84526804946fa59
Al-amin <111284227+Al-amin1911@users.noreply.github.com>
2024-04-27 07:58:39 +0100

    Delete CODE/flask web app directory

--- CODE/flask web app/main.py
+++
@@ -1,8 +0,0 @@
-from  website import create_app
-
-app = create_app()
-# Setting Entery Point
-if __name__ == '__main__':
-    app.run(debug=True)
-
- 
--- CODE/flask web app/website/views.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-
-#Define the blueprint: 'website', set its url prefix: app.url/website
-views = Blueprint('views', __name__)
-
-@views.route('/')
-def home():
-    return render_template("home.html")
-
